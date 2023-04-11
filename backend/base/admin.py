480ab5526834cca57ac8b0256f07248bd0aff4b5
anastasiialysenko0@gmail.com <anastasiialysenko0@gmail.com>
2023-04-11 21:35:07 +0300

    added lot model and created superuser

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Lot
 # Register your models here.
+
+admin.site.register(Lot)
--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Lot(models.Model):
+    STATUS_CHOICES = [
+        ('placed', 'Placed'),
+        ('active', 'Active'),
+        ('sold', 'Sold'),
+    ]
+
+    id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=255, null = False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null = False)
+    image = models.ImageField(null = True)
+    description = models.TextField(null = True)
+    is_favourite = models.BooleanField(default = False)
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null= True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10, default = 'placed')
