5bdd5bfbd45c1e67c39a72ae2d2cd78a643707e0
anastasiialysenko0@gmail.com <anastasiialysenko0@gmail.com>
2023-04-11 21:04:08 +0300

    added django_rest framework and lots/id route

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('lots/', views.getLots, name="lots"),
+    path('lots/<str:pk>/', views.getLot, name="lot"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .lots import lots
 # Create your views here.
 
+@api_view(["GET"])
 def getRoutes(request):
-    return JsonResponse("Hello", safe=False)
+    routes=[
+        "lots/",
+        "lots/<str:pk>/",
+    ]
+    return Response(routes)
 
+@api_view(["GET"])
 def getLots(request):
-    return JsonResponse(lots, safe=False)
+    return Response(lots)
+
+@api_view(["GET"])
+def getLot(request, pk):
+    lot = None
+    for i in lots:
+        if i["id"] == int(pk):
+            lot = i
+            break
+
+    return Response(lot)
