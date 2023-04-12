75bd9c93c11931ed4922a84f426074527d0bf98e
anastasiialysenko0@gmail.com <anastasiialysenko0@gmail.com>
2023-04-12 18:28:47 +0300

    added connection between backend and frontend modules and added more lots

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     "rest_framework",
+    "corsheaders",
     'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS = True
