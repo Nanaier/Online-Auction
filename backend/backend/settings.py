5629f3ded0a9d051b9d0890d16b411981e3253fd
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-04-14 23:37:37 +0300

    add folder for static files, modify settings and urls to     1. upload images to static/images folder     2. render image when clicking on 'image url' for a specific lot

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,6 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
