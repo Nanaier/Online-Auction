3887f4d18250d2cc06c98bae921f11d897569313
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-04-15 18:35:59 +0300

    set blank=True where null=True, so these fields are not required in form (admin panel) during creation

--- backend/base/models.py
+++ backend/base/models.py
@@ -11,19 +11,19 @@ class Lot(models.Model):
     ]
 
     id = models.AutoField(primary_key=True, editable=False)
-    name = models.CharField(max_length=255, null = False)
-    initial_price = models.DecimalField(max_digits=10, decimal_places=2, null = False)
-    current_price = models.DecimalField(max_digits=10, decimal_places=2, null = False)
-    image = models.ImageField(null = True)
-    auctioneer_id = models.ForeignKey(User, on_delete=models.CASCADE, null= False)
-    status = models.CharField(choices=STATUS_CHOICES, max_length=10, default = 'placed')
-    bidding_start_time = models.DateTimeField(null = True)
-    bidding_end_time = models.DateTimeField(null = True)
-    description = models.TextField(null = True)
+    name = models.CharField(max_length=255, null=False)
+    initial_price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    image = models.ImageField(null=True, blank=True)
+    auctioneer_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10, default='placed')
+    bidding_start_time = models.DateTimeField(null=True, blank=True)
+    bidding_end_time = models.DateTimeField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 class Favourites(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null= False)
-    lot_id = models.ForeignKey(Lot, on_delete=models.CASCADE, null= False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    lot_id = models.ForeignKey(Lot, on_delete=models.CASCADE, null=False)
