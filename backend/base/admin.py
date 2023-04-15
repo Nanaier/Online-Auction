4489d40239b3436a52729800c1608845110f537a
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-04-15 16:24:01 +0300

    change Lot model, add Favourites model

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Lot
+from .models import Lot, Favourites
 # Register your models here.
 
 admin.site.register(Lot)
+admin.site.register(Favourites)
--- backend/base/models.py
+++ backend/base/models.py
@@ -12,9 +12,18 @@ class Lot(models.Model):
 
     id = models.AutoField(primary_key=True, editable=False)
     name = models.CharField(max_length=255, null = False)
-    price = models.DecimalField(max_digits=10, decimal_places=2, null = False)
+    initial_price = models.DecimalField(max_digits=10, decimal_places=2, null = False)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2, null = False)
     image = models.ImageField(null = True)
-    description = models.TextField(null = True)
-    is_favourite = models.BooleanField(default = False)
-    user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null= True)
+    auctioneer_id = models.ForeignKey(User, on_delete=models.CASCADE, null= False)
     status = models.CharField(choices=STATUS_CHOICES, max_length=10, default = 'placed')
+    bidding_start_time = models.DateTimeField(null = True)
+    bidding_end_time = models.DateTimeField(null = True)
+    description = models.TextField(null = True)
+
+    def __str__(self):
+        return self.name
+
+class Favourites(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null= False)
+    lot_id = models.ForeignKey(Lot, on_delete=models.CASCADE, null= False)
