b4280dea098fe75877782cbb111931b18a79feb8
muratalkhadam <muratalkhadam@gmail.com>
2023-04-18 16:07:56 +0300

    AUTH_USER_MODEL specification added for custom user

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,3 +131,4 @@ MEDIA_ROOT = 'static/images'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'base.User'
