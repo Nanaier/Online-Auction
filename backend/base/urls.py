fe63b8178b9d28cfc1279c0af7036f45503301c3
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-04-20 23:32:29 +0300

    add JWT Customization     - now token contains username and email

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
 from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
 
 urlpatterns = [
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('lots/', views.getLots, name="lots"),
     path('lots/<str:pk>/', views.getLot, name="lot"),
--- backend/base/views.py
+++ backend/base/views.py
@@ -5,6 +5,20 @@ from rest_framework.response import Response
 from .models import Lot
 from .serializers import LotSerializer
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        return data
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 @api_view(["GET"])
 def getRoutes(request):
     routes=[
