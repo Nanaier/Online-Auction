ff3aa42994b04f5c2bb10c3f7e7d886481cddad1
Lidia Makarchuk <makarchuk.lidiia@gmail.com>
2023-04-20 23:08:49 +0300

    add rest_framework_simplejwt

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 urlpatterns = [
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('lots/', views.getLots, name="lots"),
     path('lots/<str:pk>/', views.getLot, name="lot"),
