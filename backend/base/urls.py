251a84074ab249a8dabde006a6ee3997b64026ff
anastasiialysenko0@gmail.com <anastasiialysenko0@gmail.com>
2023-04-21 20:19:42 +0300

    register added

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('', views.getRoutes, name="routes"),
+    path('users/register/', views.registerUser, name='register' ),
     path('user/profile/', views.getUserProfile, name="user-profile"),
     path('users/', views.getUsers, name="users"),
     path('lots/', views.getLots, name="lots"),
--- backend/base/views.py
+++ backend/base/views.py
@@ -9,6 +9,9 @@ from .serializers import LotSerializer, UserSerializer, UserSerializerWithToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
@@ -23,13 +26,26 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-@api_view(["GET"])
-def getRoutes(request):
-    routes=[
-        "lots/",
-        "lots/<str:pk>/",
-    ]
-    return Response(routes)
+
+@api_view(["POST"])
+def registerUser(request):
+    data = request.data
+
+    try:
+        user= User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password']),
+            phone_number = data['phone_number']
+        )
+
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
