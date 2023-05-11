5b2f42fd84df08efe76de3f4468e129d8275dec9
anastasiialysenko0@gmail.com <anastasiialysenko0@gmail.com>
2023-05-11 20:29:30 +0300

    profile url in backend changed

--- backend/base/models.py
+++ backend/base/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     id = models.AutoField(primary_key=True, editable=False)
-    phone_number = models.CharField(max_length=20, unique=True, null=False)
+    phone_number = models.CharField(max_length=20, unique=True, null=True)
     balance = models.DecimalField(max_digits=15, decimal_places=3, default=0, null=False)
 
 class Lot(models.Model):
--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.registerUser, name='register' ),
-    path('users/profile/', views.getUserProfile, name="user-profile"),
+    path('profile/', views.getUserProfile, name="user-profile"),
     path('', views.getUsers, name="users"),
     
 ]
