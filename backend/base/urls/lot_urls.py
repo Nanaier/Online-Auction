ddec77c50262858625dbebeb19134c257b34d2d1
muratalkhadam <muratalkhadam@gmail.com>
2023-05-12 11:34:17 +0300

    endpoints for bid actions added

--- backend/base/urls/lot_urls.py
+++ backend/base/urls/lot_urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from base.views import lot_views as views
+from ..views import lot_views as views
 
 urlpatterns = [
     path('', views.getLots, name="lots"),
@@ -8,4 +8,8 @@ urlpatterns = [
     path('<str:pk>/update/', views.updateLot, name="updateLot"),
     path('<str:pk>/favourite/', views.favouriteLot, name="favouriteLot"),
     path('<str:pk>/', views.getLot, name="lot"),
+
+    path('<str:pk>/bid/create/', views.createBid, name="createBid"),
+    path('<str:pk>/bids/', views.getLotBids, name="lotBids"),
+    path('<str:pk>/bid/last/', views.getLastBid, name="lastBid"),
 ]
