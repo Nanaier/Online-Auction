77a5262c9a7416937cb1be5b3141074185e6e4f8
muratalkhadam <muratalkhadam@gmail.com>
2023-05-12 11:32:19 +0300

    import fixed

--- backend/base/views/favourite_views.py
+++ backend/base/views/favourite_views.py
@@ -3,8 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from base.models import Lot, Favourites
-from base.serializers import LotSerializer
+from ..models import Lot, Favourites
+from ..serializers import LotSerializer
 
 from rest_framework import status
 
